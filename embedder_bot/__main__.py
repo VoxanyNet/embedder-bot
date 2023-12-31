7ca3a169333b44c669af2fe8c1814eed922e35dd
VoxanyNet <contact@voxany.net>
2023-12-30 23:06:14 -0500

    added compose file

--- embedder_bot/__main__.py
+++ embedder_bot/__main__.py
@@ -2,7 +2,7 @@ import os
 
 from embedder_bot import bot
 
-DISCORD_TOKEN = os.environ["EMBEDDER_BOT_DISCORD_TOKEN"]
+DISCORD_TOKEN = os.environ["BOT_DISCORD_TOKEN"]
 bot = bot.EmbedderBot()
 
 bot.run(DISCORD_TOKEN)
