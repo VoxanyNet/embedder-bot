8385164444ebdf008a0c5faeed52d2ce5ae43a8e
VoxanyNet <contact@voxany.net>
2024-01-06 22:24:01 -0500

    fixed dict query bug

--- embedder_bot/utils.py
+++ embedder_bot/utils.py
@@ -4,7 +4,7 @@ def find_one(query: dict, items: List[dict]) -> Optional[dict]:
     """Search through a list of dicts and return the first dict with a given value"""
     
     for item in items:
-        if item | query:
+        if query.items() <= item.items():
             return item 
     
     return None
